cf4d934d1ff85124de48ca1b616e2c4c150a7d0b
Ram Ji Kanaujia <58133014+ramjikanaujia@users.noreply.github.com>
2021-09-15 15:19:19 +0530

    Add files via upload

---
+++ Algorithms/Recursion/the-power-sum.py
@@ -0,0 +1,18 @@
+
+
+import math
+
+def get_power_sum(s, p, n):
+    count = s - (n ** p)
+    print (count)
+    if count == 0:
+        return 1
+    elif count < 0:
+        return 0
+    else:
+        return get_power_sum(count, p, n + 1) + get_power_sum(s, p, n + 1)
+
+s = int(input().strip())
+p = int(input().strip())
+n = 1
+print (get_power_sum(s, p, n))
