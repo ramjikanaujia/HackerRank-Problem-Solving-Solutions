d1111ffe5ed822c6b72fe90b083c81d64ec3f3d2
Ram Ji Kanaujia <58133014+ramjikanaujia@users.noreply.github.com>
2021-09-15 15:34:21 +0530

    Add files via upload

---
+++ Data Structures/Linked List/insert-a-node-at-the-head-of-a-linked-list.py
@@ -0,0 +1,34 @@
+
+
+class SinglyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(head):
+    while head:
+        print (head.data)
+        head = head.next
+
+def insertNodeAtHead(llist, data):
+    if llist is None:
+        return SinglyLinkedListNode(data)
+    else:
+        next_temp = llist
+        llist = SinglyLinkedListNode(data)
+        llist.next = next_temp
+    return llist
+
+if __name__ == '__main__':
+    llist_count = int(input())
+    llist = SinglyLinkedList()
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    print_singly_linked_list(llist.head)
---
+++ Data Structures/Linked List/insert-a-node-at-the-tail-of-a-linked-list.py
@@ -0,0 +1,46 @@
+
+
+class SinglyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(head):
+    while head:
+        print (head.data)
+        head = head.next
+
+def insertNodeAtTail(head, data):
+    if head is None:
+        return SinglyLinkedListNode(data)
+    elif head.next is None:
+        head.next = SinglyLinkedListNode(data)
+    else:
+        temp = head
+        while temp.next:
+            temp = temp.next
+        temp.next = SinglyLinkedListNode(data)
+    return head
+
+def insertNodeAtTailRecursive(head, data):
+    if head is None:
+        return SinglyLinkedListNode(data)
+    elif head.next is None:
+        head.next = SinglyLinkedListNode(data)
+    else:
+        insertNodeAtTail(head.next, data)
+    return head
+
+if __name__ == '__main__':
+    llist_count = int(input())
+    llist = SinglyLinkedList()
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+    print_singly_linked_list(llist.head)
